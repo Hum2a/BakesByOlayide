0ebe325e59e314d1d50778bb660007ddaf91ba92
Humza Butt <Humzab1711@hotmail.com>
2025-06-02 18:48:12 +0100

    Update FAQ component with revised order notice periods, deposit requirements, and collection instructions for improved clarity. Enhanced guidance on cake storage and handling, while maintaining original content structure.

--- src/components/pages/FAQ.js
+++ src/components/pages/FAQ.js
@@ -91,26 +91,58 @@ const FAQ = () => {
           <div className="faq-content">
             <div className="faq-text">
               <p><b>How far in advance should I make my order?</b><br />
-              The shortest notice we accept for single-tier celebration cakes is 5 days. Multi-tier celebration cakes require a minimum of 2 weeks' notice. Wedding cakes require a minimum of 8 weeks' notice (including sampling and consultation). Large cupcakes, brownie or cookie orders (greater than 36, fewer than 72 portions) require a minimum of 1 week's notice. Corporate or very large orders (greater than 72 individual portions) require a minimum of 6 weeks' notice.</p>
+              The shortest notice we accept for all items on this website is 4 days.
+              <br />
+              <br />
+              Multi-tier celebration cakes require a minimum of 2 weeks' notice.
+              <br />
+              <br />
+              Enquiry only orders (larger than order maximums, multi-tiers or number/letter cakes) require a minimum notice of 2 weeks.
+              <br />
+              <br />
+              Orders related to weddings with consultations require a minimum of 6 weeks’ notice. This includes sampling and consultation, alongside finalising designs and delivery of said cake.
+              </p>
 
               <p><b>Do orders require deposits?</b><br />
-              Orders valued over £50 require a 50% deposit upon order. Once your deposit is paid, we'll process your order. We'll send you pictures of your order before your pick-up time so allow for any custom changes. When you're happy with everything, you'll pay the second half of your payment via invoice or via contactless.</p>
+              For orders made directly through the website, no deposit is required. The full amount is paid in full upon order.
+              <br />
+              <br />
+              For orders made via enquiry (larger than order maximums, unconventional cake orders or multi-tiers), a 50% non-refundable deposit is due within 2 days of the order’s confirmation. This is required to reserve the date of your order.
+              <br />
+              <br />
+              Once your deposit is paid via invoice, we’ll process your order. We’ll send you pictures before your pick-up time comes to allow for any cosmetic changes. When you’re happy with everything, you’ll pay the second half of your payment via invoice or via card terminal before receiving your order
+              <br />
+              <br />
+              Once your deposit is paid via invoice, we’ll process your order. We’ll send you pictures before your pick-up time comes to allow for any cosmetic changes. When you’re happy with everything, you’ll pay the second half of your payment via invoice or via card terminal before receiving your order
+              </p>
 
               <p>Orders will not be accessible to you until the entire payment has been processed and received.</p>
 
               <p><b>What payment methods do you accept?</b><br />
-              We currently accept card, contactless and PayPal.</p>
+              We currently accept card, contactless and PayPal.
+              </p>
 
               <p><b>Do you deliver?</b><br />
-              Currently we do not offer delivery outside of postal products, corporate events and weddings. We're working on expanding to deliver to all.</p>
+              Currently we do not offer delivery outside of postal products or weddings. We’re working on expanding to deliver to all.
+              </p>
 
               <p><b>When can I collect my order?</b><br />
-              When you commission your bakes, you will be given the option to schedule a pick-up time within our regular open hours. Within 24 hours of your planned pick-up time, you'll get our address. If you cannot attend your scheduled time, you will be contacted when your order is ready alongside our team.</p>
+                When you commission your bakes, you will be given the option to schedule a pick-up time within our regular open hours. Within 24 hours before your planned pick-up time, you’ll be sent our address. If you opt out of choosing a schedule time, you will be contacted when your order is ready alongside our address. 
+                <br />
+                <br />
+                Our bakery is currently located in a house with step only access. If you need assistance in loading your car with your order, we’re happy to help.</p>
 
-              <p>Our bakery is currently located in a house with step only access. If you need assistance in handling your order with your order, we're happy to help.</p>
+              <p><b>How Should I collect and store large cakes?</b><br />
+                When you pick up your cakes and bakes, it’s highly recommended that you avoid stacking boxes on top of each other. Hold your large cake from the base and avoid picking it up with the edges. This is to minimize potential damage to your order. You should avoid using the heating in the car during colder months. Ensure you air-condition your car when the weather is warmer.                <br />
+                <br />
+                When you arrive home or at your venue, store the cake in a fridge or a cool, dry place. If the cake has whipped cream, you must store it in the fridge. When you’re ready to eat, leave the cake in a cool, dry environment for at least 30 minutes before serving.
+                <br />
+                <br />
+                To preserve the texture of the cake for longer, when you put the cake away, use clingfilm to cover the exposed edge of the cake and reuse the provided packaging if possible. If the box is too tall for your fridge, wrap the cake in entirety before placing in the fridge.
+                </p>
 
               <p><b>Do you make fondant or 3D cakes?</b><br />
-              Bakes by Olayide is a fondant-free zone: our founder isn't a fan of it. We don't make realistic cakes either. (There are many great bakers and cake artists who do though!)</p>
+              Bakes by Olayide is a fondant free zone- our founder isn’t a fan of it. We don’t make realistic cakes either. There are many great bakers and cake artists who do though!</p>
 
               <p><b>Where can I find information on allergens?</b><br />
               You can find information on our handling of allergens <a href="/guides/dietary-requirements" className="faq-link">here</a>. We also have a table of all allergens in our bakes <a href="/allergens" className="faq-link">here</a>.</p>
